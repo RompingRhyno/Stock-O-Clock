487e374ec33e26af6a0603aba614f9b2b27d1c40
Alex <cyberandpower@gmail.com>
2023-11-14 09:04:41 -0800

    Logan did an oopsie and deleted some code, merging and re-adding deleted code

--- scripts/settings.js
+++ scripts/settings.js
@@ -5,6 +5,16 @@ var user = firebase.auth();
 // var settings = db.collection("users").doc(user.uid)
 // .collection("settings").doc("settings");
 
+const toggleButtonContainer = document.querySelectorAll('.toggle-button');
+const toggleButton = document.querySelectorAll('.toggle-button i');
+
+for (let i = 0; i < toggleButton.length; i++) {
+    toggleButtonContainer[i].addEventListener('click', function () {
+        toggleButton[i].classList.toggle('fa-toggle-off');
+        toggleButton[i].classList.toggle('fa-toggle-on');
+    });
+};
+
 var currentUser;               //points to the document of the user who is logged in
 function notificationToggle() {
     db.collection("users")

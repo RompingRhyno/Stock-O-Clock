4e89741ad3bb5fe003307598339070ed0f3989af
Alex Huynh <cyberandpower@gmail.com>
2023-12-04 03:26:36 -0800

    Made the displayFridges function activate only once and did some styles stuff on the list cards, I can't get the form to add the fridges properly.

--- scripts/fridge.js
+++ scripts/fridge.js
@@ -1,48 +1,62 @@
+var isClicked = false;
 
 function displayFridges() {
     let cardTemplate = document.getElementById("fridgeCardTemplate");
-    db.collection("users").doc(userId).collection("fridges").get().then((allFridges) => {
-        allFridges.forEach((doc) => {
-            var fridgeName = doc.data().fridgeName;
+    if (isClicked === false) {
+        isClicked = true;
+        db.collection("users").doc(userId).collection("fridges").get().then((allFridges) => {
+            allFridges.forEach((doc) => {
+                var fridgeName = doc.data().fridgeName;
+                console.log(fridgeName);
 
-            let newcard = cardTemplate.content.cloneNode(true).firstElementChild;
+                let newcard = cardTemplate.content.cloneNode(true).firstElementChild;
 
-            // Update fridge name on card
-            newcard.querySelector('.card-title').innerHTML = fridgeName;
+                // Update fridge name on card
+                newcard.querySelector('.card-title').innerHTML = fridgeName;
 
-            //attach to gallery, Example: "fridges-go-here"
-            document.getElementById("fridges-go-here").appendChild(newcard);
+                //attach to gallery, Example: "fridges-go-here"
+                document.getElementById("fridges-go-here").appendChild(newcard);
+            })
         })
-    })
+    }
 }
 
 // Write fridge info from form to firebase (Not Functional atm)
 var fridgeForm = document.getElementById('myForm-j');
 fridgeForm.addEventListener('submit', function () {
     const docRef = db.collection("users").doc(userId);
-    docRef.get().then(function(doc) {
+    docRef.get().then(function (doc) {
         if (doc.exists) {
             var fridgesRef = db.collection("users").doc(userId).collection("fridges");
-            var fridgeNameInput = document.getElementById("fridgeID").value;
+            var fridgeNameInput = document.getElementById("fridgeName").value;
+            var fridgeIDInput = document.getElementById("fridgeID").value;
 
-            console.log(fridgesRef)
-            console.log(fridgeNameInput)
+            console.log(fridgesRef);
+            console.log(fridgeNameInput);
 
-            fridgesRef.set(fridgeNameInput)
-
-                .then(function () {
-                    //console.log('Document written with ID: ', docRef.id);
-                    // Reload the page after the write is successful
-                    // location.reload(); // This will trigger a page refresh
+            fridgesRef.doc(fridgeIDInput).set({
+                fridgeName: fridgeNameInput
+            })
+                .then(() => {
+                    console.log("Document successfully written!");
                 })
-                .catch(function (error) {
-                    console.error('Error adding document: ', error);
+                .catch((error) => {
+                    console.error("Error writing document: ", error);
                 });
 
-            
+            // .then(function () {
+            //     //console.log('Document written with ID: ', docRef.id);
+            //     // Reload the page after the write is successful
+            //     // location.reload(); // This will trigger a page refresh
+            // })
+            // .catch(function (error) {
+            //     console.error('Error adding document: ', error);
+            // });
+
+
             // Clear the form fields
             document.getElementById('fridgeName').value = '',
-            document.getElementById('fridgeID').value = '';
+                document.getElementById('fridgeID').value = '';
             closeForm();
         }
     })
